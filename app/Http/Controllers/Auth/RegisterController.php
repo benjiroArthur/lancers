d27b3e59931f93493b4fcc8870cd15582404ad3d
Benjamin Arthur <40395048+benjiroArthur@users.noreply.github.com>
2020-05-21 01:04:05 +0000

    vue and npm installed

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Client;
+use App\Freelancer;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use App\Role;
 use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
@@ -50,9 +53,9 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'email' => 'required|string|email|max:255|unique:users|unique:clients|unique:freelancers',
+            'password' => 'required|string|min:8|confirmed',
+            'user_type' => 'required'
         ]);
     }
 
@@ -64,9 +67,18 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
-            'name' => $data['name'],
+        $data2['email'] = $data['email'];
+        $role = Role::where('name', $data['user_type'])->firstOrFail();
+        if($data['user_type'] === 'client'){
+
+            $newUser = Client::create($data2);
+        }
+        else{
+            $newUser = Freelancer::create($data2);
+        }
+        return $newUser->user()->create([
             'email' => $data['email'],
+            'role_id' => $role->id,
             'password' => Hash::make($data['password']),
         ]);
     }
--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,146 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import Vuetify from 'vuetify';
+Vue.use(Vuetify);
+
+import {RotateSquare2} from 'vue-loading-spinner';
+
+window.RotateSquare2 = RotateSquare2;
+
+
+Vue.use(VueRouter);
+
+require('@fortawesome/fontawesome-free/js/all.js');
+require('bootstrap-table/dist/bootstrap-table.min.css');
+require('bootstrap-table/dist/bootstrap-table.js');
+
+require('tableexport.jquery.plugin');
+require('bootstrap-table/dist/extensions/export/bootstrap-table-export.min.js');
+require('bootstrap-table/dist/extensions/print/bootstrap-table-print.min.js');
+
+require('bootstrap-table/dist/extensions/filter-control/bootstrap-table-filter-control.min.css');
+require('bootstrap-table/dist/extensions/filter-control/bootstrap-table-filter-control.min.js');
+
+import * as VueGoogleMaps from 'vue2-google-maps';
+
+Vue.use(VueGoogleMaps, {
+    load: {
+        key: 'YOUR_API_TOKEN',
+        libraries: 'places', // This is required if you use the Autocomplete plugin
+        // OR: libraries: 'places,drawing'
+        // OR: libraries: 'places,drawing,visualization'
+        // (as you require)
+
+        //// If you want to set the version, you can do so:
+        // v: '3.26',
+    },});
+
+
+//import {Form, HasError, AlertError} from 'vform';
+import {
+    Form,
+    HasError,
+    AlertError,
+    AlertErrors,
+    AlertSuccess
+} from 'vform';
+
+window.Form = Form;
+Vue.component(HasError.name, HasError);
+Vue.component(AlertError.name, AlertError);
+Vue.component(AlertErrors.name, AlertErrors);
+Vue.component(AlertSuccess.name, AlertSuccess);
+
+
+/*Vue.component(HasError.name, HasError);
+Vue.component(AlertError.name, AlertError);*/
+
+import VueProgressBar from 'vue-progressbar'
+
+Vue.use(VueProgressBar, {
+    color: 'rgb(143, 255, 199)',
+    failedColor: 'red',
+    height: '2px'
+});
+
+
+// Import this component
+import datePicker from 'vue-bootstrap-datetimepicker';
+window.datePicker = datePicker;
+
+jQuery.extend(true, jQuery.fn.datetimepicker.defaults,{
+    icons:{
+        time: 'far fa-clock',
+        date: 'far fa-calendar',
+        up: 'fas fa-arrow-up',
+        down: 'fas fa-arrow-down',
+        previous: 'fas fa-chevron-left',
+        next: 'fas fa-chevron-right',
+        today: 'fas fa-calendar',
+        clear: 'far fa-trash-alt',
+        close: 'far fa-times-circle',
+
+    }
+});
+
+
+import Swal from 'sweetalert2';
+window.Swal = Swal;
+
+
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    onOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer);
+        toast.addEventListener('mouseleave', Swal.resumeTimer);
+    }
+});
+window.toast = Toast;
+
+import VueSweetalert2 from 'vue-sweetalert2';
+Vue.use(VueSweetalert2);
+
+import moment from 'moment';
+Vue.filter('uptext', function(text){
+    return text.charAt(0).toUpperCase() + text.slice(1);
+});
+Vue.filter('myDate', function(text){
+    return moment(text).format('MMMM Do YYYY');
+});
+
+Vue.filter('fromdate', function(text){
+    return moment(text).fromNow();
+});
+
+
+router.beforeResolve((to, from, next)=>{
+    if(to.path){
+
+    }
+    next();
+});
+
+router.afterEach(()=>{
+
+});
+
+import VueLazyload from 'vue-lazyload';
+
+Vue.use(VueLazyload);
+
+import VueCropper from 'vue-cropperjs';
+import 'cropperjs/dist/cropper.css';
+Vue.component(VueCropper);
+
+
+
+
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -21,6 +161,24 @@ window.Vue = require('vue');
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
+
+let routes = [
+
+];
+
+const router = new VueRouter({
+    mode:'history',
+    routes
+
+});
+
+window.Fire = new Vue();
+
+import VueRouter from 'vue-router';
+import axios from "axios";
+import { VuejsDatatableFactory } from 'vuejs-datatable';
+
+Vue.use(VueRouter, axios, VuejsDatatableFactory);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -29,4 +187,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    router,
+    vuetify: new Vuetify(),
 });
