fea83d4a2d7747a6e0ecad613c80be58e8067425
Precaution-23 <60000248+Precaution-23@users.noreply.github.com>
2020-05-22 13:05:52 +0000

    made migration on the jobs table

--- database/migrations/2020_05_21_233017_change_category_id_in_jobs_table.php
+++ database/migrations/2020_05_21_233017_change_category_id_in_jobs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateJobsTable extends Migration
+class ChangeCategoryIdInJobsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,9 @@ class CreateJobsTable extends Migration
      */
     public function up()
     {
-        Schema::create('jobs', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->unsignedBigInteger('job_category_id');
-            $table->timestamps();
+
+        Schema::table('jobs', function (Blueprint $table) {
+            $table->renameColumn('category_id', 'job_category_id');
         });
     }
 
@@ -28,6 +26,8 @@ class CreateJobsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('jobs');
+        Schema::table('jobs', function (Blueprint $table) {
+            //
+        });
     }
 }
