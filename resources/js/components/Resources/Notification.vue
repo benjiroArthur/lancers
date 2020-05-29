56385014f2475c50f346ab225ad7e19f73004cf6
Benjamin Arthur <40395048+benjiroArthur@users.noreply.github.com>
2020-05-29 12:15:43 +0000

    Create Notification.vue

---
+++ resources/js/components/Resources/Notification.vue
@@ -0,0 +1,26 @@
+<template>
+    <li class="nav-item dropdown ml-4">
+        <a class="nav-link count-indicator text-white" id="notificationDropdown" href="#" data-toggle="dropdown">
+            <i class="fas fa-bell text-white h4"></i>
+            <span class="badge badge-danger">9</span>
+        </a>
+
+
+        <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list pb-0" aria-labelledby="notificationDropdown">
+            <a class="dropdown-item py-3 border-bottom">
+                <p class="mb-0 font-weight-medium float-left">You have 4 new notifications </p>
+                <span class="badge badge-pill badge-primary float-right">View all</span>
+            </a>
+        </div>
+    </li>
+</template>
+
+<script>
+    export default {
+        name: "Notification"
+    }
+</script>
+
+<style scoped>
+
+</style>
