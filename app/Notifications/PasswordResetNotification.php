8fd93864abf0c714d01bfddd481734642bfe79aa
Benjamin Arthur <40395048+benjiroArthur@users.noreply.github.com>
2020-05-31 22:22:45 +0000

    commit new

--- app/Notifications/PasswordResetNotification.php
+++ app/Notifications/PasswordResetNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Facades\Lang;
 
-class PasswordResetNotification extends Notification implements ShouldQueue
+class PasswordResetNotification extends Notification
 {
     use Queueable;
     public $token;
