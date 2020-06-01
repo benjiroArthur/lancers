0f2fdd12794044ed821706b39e928b4006fb078c
Kwame Swift <fuakyeakyempim.charles@gmail.com>
2020-06-01 14:17:01 +0000

    Update

--- resources/js/components/Chat/Chat.vue
+++ resources/js/components/Chat/Chat.vue
@@ -28,7 +28,7 @@
                             <span class="direct-chat-timestamp float-right">23 Jan 2:00 pm</span>
                         </div>
                         <!-- /.direct-chat-infos -->
-                        <img class="direct-chat-img" src="../dist/img/user1-128x128.jpg" alt="Message User Image">
+                        <img class="direct-chat-img" src="" alt="Message User Image">
                         <!-- /.direct-chat-img -->
                         <div class="direct-chat-text">
                             Is this template really for free? That's unbelievable!
@@ -44,7 +44,7 @@
                             <span class="direct-chat-timestamp float-left">23 Jan 2:05 pm</span>
                         </div>
                         <!-- /.direct-chat-infos -->
-                        <img class="direct-chat-img" src="../dist/img/user3-128x128.jpg" alt="Message User Image">
+                        <img class="direct-chat-img" src="" alt="Message User Image">
                         <!-- /.direct-chat-img -->
                         <div class="direct-chat-text">
                             You better believe it!
@@ -60,7 +60,7 @@
                     <ul class="contacts-list">
                         <li>
                             <a href="#">
-                                <img class="contacts-list-img" src="../dist/img/user1-128x128.jpg">
+                                <img class="contacts-list-img" src="">
 
                                 <div class="contacts-list-info">
                           <span class="contacts-list-name">
